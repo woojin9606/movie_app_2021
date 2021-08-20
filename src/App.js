7c7f97b124a856da4d22bd9bdc9364b1e7ed161a
woojin9606 <qordnwls11@gmail.com>
2021-08-20 16:39:24 +0900

    2.2 Dynamic Component Generation

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import React from "react";
 
-function Food({ fav }){
-  return <h1>I love { fav }</h1>
+function Food({ name, picture }){
+  return <div>
+    <h2>I love { name }</h2>
+    <img src={picture} />
+  </div>
+
 }
+
+const foodILike = [{
+  name : "kimchi",
+  image : "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.QKNL3H-b8pGMgfg77Ig8BAHaE8%26pid%3DApi%26h%3D160&f=1"
+},
+{
+  name : "bulgogi",
+  image : "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.JfOZtS5v4wLjtpacTImfVwHaEK%26pid%3DApi%26h%3D160&f=1"
+}]
 
 function App() {
   return (
     <div>
-      <h1>Hello!!!!!!!!</h1>
-      <Food fav="kimchi" />
-      <Food fav="ramen" />
-      <Food fav="samgiopsal" />
-      <Food fav="chukume" />
+      {foodILike.map (dish =>(
+        <Food name={dish.name} picture={dish.image}/>
+      ))}
     </div>
     );
 }
